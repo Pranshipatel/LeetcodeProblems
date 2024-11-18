cb51c0f980b3493c1f909ef36a64e023e105001b
Pranshipatel <157210388+Pranshipatel@users.noreply.github.com>
2024-11-18 15:32:51 +0530

    Time: 5 ms (72.21%), Space: 43.6 MB (88.82%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-       int rev = 0;
-       int copy = x;
-       while(x > 0){
-        int rem = x % 10 ;
-        rev = rev*10 + rem;
-        x = x/10;
-       }
-
-       if(rev == copy)return true;
-       return false;
+        int rev = 0;
+        int copy = x;
+        while(x>0){
+            int rem = x %10;
+            rev = rev*10+rem;
+            x = x/10;
+        }
+        if(copy == rev){
+            return true;
+        }
+        return false;
     }
 }
