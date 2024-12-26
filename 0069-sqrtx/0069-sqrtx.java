ec33b0079fc045a6970ecfa354c4e2217085979f
Pranshipatel <157210388+Pranshipatel@users.noreply.github.com>
2024-12-26 20:43:09 +0530

    Time: 1 ms (86.92%), Space: 41.3 MB (14.36%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
-       return (int)Math.sqrt(x);
+        if(x ==  0 || x == 1)return x;
+        int ans = 0,  low = 0 , high = x;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(mid <= x/mid){ 
+                ans = mid;
+                low = mid+1;
+            }
+            else high = mid -1;
+        }
+        return ans;
     }
 }
