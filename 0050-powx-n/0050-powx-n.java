5468806ccceda4c8cf67556073a9832e8b81fffa
Pranshipatel <157210388+Pranshipatel@users.noreply.github.com>
2024-12-28 18:46:06 +0530

    Time: 0 ms (100%), Space: 42.2 MB (58.8%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-       double ans =  Math.pow(x,n);
+       double ans = 1.0;
+       double pow = n;
+       if(pow  < 0){
+        pow = pow * -1;
+       }
+       while(pow > 0 ){
+        if(pow % 2 == 0){
+         x = x*x;
+        pow = pow/2;
+        }
+        else{
+            ans *= x;
+            pow = pow - 1;
+        };
+        
+       }
+       if(n < 0){
+        ans = 1/ans;
+       }
        return ans;
     }
+    
 }
