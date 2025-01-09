79bc7ca4d7c42f3a775bf67dcb56b4e3813eda64
Pranshipatel <157210388+Pranshipatel@users.noreply.github.com>
2025-01-10 00:22:47 +0530

    Time: 0 ms (100%), Space: 42.9 MB (73.86%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,14 +4,10 @@ class Solution {
         while(start <= end){
             int mid = start + (end -start)/2;
             if(nums[mid] == target)return mid;
-            else if(nums[mid]> target) end = mid -1;
+            else if(nums[mid]>target) end = mid -1;
             else start = mid+1;
         }
         
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i]<target)continue;
-            else return i;
-        }
-        return nums.length;
+        return start;
     }
 }
